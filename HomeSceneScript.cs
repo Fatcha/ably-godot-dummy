c5ff3cb158a344a525491a9e6f96fe270d75a8cc
Brieuc Barthélemy <bri@fatcha.be>
2025-02-23 20:40:41 +0100

    Minimal Version for testing

--- HomeSceneScript.cs
+++ HomeSceneScript.cs
@@ -2,42 +2,57 @@ using Godot;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Helpers;
 using IO.Ably;
 using IO.Ably.Realtime;
+using Newtonsoft.Json.Linq;
 
 public partial class HomeSceneScript : Control
 {
     private AblyRealtime _ably;
     private bool _isConnected = false;
     
+    private string ApiUrl = "";
+    private string ChannelToSubscribe = "";
+
     private RichTextLabel _logRichTextLabel;
-    
+
     public override void _Ready()
     {
         _logRichTextLabel = GetNode<RichTextLabel>("%LogRichTextLabel");
-        
+
         ConnectToAbly();
-        
+
         AddLogInTextArea("Ably connection Testing!");
     }
-    
+
     private async Task<object> AblyCallback(TokenParams tokenParams)
     {
-        string newToken = "TOKEN_SERVERSIDE_GENERATED";
+
+        HttpRequestClient client = new HttpRequestClient();
+        string token = null;
+        await client.GetRequest(ApiUrl, response =>
+        {
+            var json = JToken.Parse(response);
+            token = json["payload"]?.ToString();
+            GD.Print($"[Ably] 🔑 Received new token: {token}");
+        });
+
+        string newToken = token;
         return await Task.FromResult(new TokenDetails { Token = newToken });
     }
-    
+
     private async void ConnectToAbly()
     {
         try
         {
             _ably = new AblyRealtime(new ClientOptions()
             {
-                AuthCallback = AblyCallback, 
+                AuthCallback = AblyCallback,
                 AutoConnect = true,
                 CustomContext = SynchronizationContext.Current,
                 AutomaticNetworkStateMonitoring = false,
-                
+
                 LogHandler = new CustomLogger(),
                 LogLevel = IO.Ably.LogLevel.Debug
             });
@@ -46,6 +61,7 @@ public partial class HomeSceneScript : Control
             {
                 AddLogInTextArea("✅ Connected to Ably!");
                 _isConnected = true;
+                SubscribeToCharacterChannel();
             });
 
             _ably.Connection.On(ConnectionEvent.Disconnected, (stateChange) =>
@@ -64,10 +80,24 @@ public partial class HomeSceneScript : Control
             AddLogInTextArea($"❌ Error initializing Ably: {ex.Message}");
         }
     }
+
+    private void SubscribeToCharacterChannel()
+    {
+        AddLogInTextArea($"🛩️ Trying to subscribe to channel: private:character.M");
+        var channel = _ably.Channels.Get(ChannelToSubscribe);
     
-    public void AddLogInTextArea(string message)
+        channel.Subscribe((message) =>
+        {
+            AddLogInTextArea($"✅ Message received: {message}");
+            GD.Print("Message received: " );
+            GD.Print(message);
+        });
+    }
+
+public void AddLogInTextArea(string message)
     {
         _logRichTextLabel.Text += message + "\n";
+        GD.Print(message);
     }
      
 }
---
+++ HttpRequestClient.cs
@@ -0,0 +1,106 @@
+﻿using System.Linq;
+using Newtonsoft.Json.Linq;
+
+ 
+    using System.Linq;
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Godot;
+
+    namespace Helpers
+    {
+        public class HttpRequestClient
+        {
+            private string url = "";
+            public enum RequestContentType
+            {
+                Json,
+                FormData
+            }
+
+            public delegate void ResponseCallback(string responseBody);
+
+            // Méthode asynchrone pour effectuer une requête HTTP GET
+            public async Task GetRequest(string endpoint, ResponseCallback callback)
+            {
+               
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + endpoint);
+                request.Method = "GET";
+                await SendRequest(request, callback, null, RequestContentType.Json);
+            }
+
+            // Méthode asynchrone mise à jour pour supporter Json ou FormData
+            public async Task PostRequest(string endpoint, string data, RequestContentType contentType,
+                ResponseCallback callback)
+            {
+                 
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + endpoint);
+                request.Method = "POST";
+
+                // Définit le ContentType en fonction du type de contenu spécifié
+                switch (contentType)
+                {
+                    case RequestContentType.Json:
+                        request.ContentType = "application/json";
+                        break;
+                    case RequestContentType.FormData:
+                        request.ContentType = "application/x-www-form-urlencoded";
+                        break;
+                }
+
+                await SendRequest(request, callback, data, contentType);
+            }
+
+            // Ajoute un paramètre pour le type de contenu à SendRequest
+            private async Task SendRequest(HttpWebRequest request, ResponseCallback callback, string data, RequestContentType contentType)
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+                // Ajoute les headers personnalisés à la requête
+                request.Accept = "application/json";
+                
+
+                // Ajoute le corps de la requête pour POST
+                if (data != null)
+                {
+                    using (var streamWriter = new StreamWriter(await request.GetRequestStreamAsync()))
+                    {
+                        streamWriter.Write(data);
+                    }
+                }
+
+                try
+                {
+                    using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        var responseBody = await reader.ReadToEndAsync();
+                        callback?.Invoke(responseBody);
+                    }
+                }
+                catch (WebException e)
+                {
+                    GD.Print("Error in HTTP request: ", e.Message);
+                    if (e.Response is HttpWebResponse errorResponse)
+                    {
+                        using (Stream responseStream = errorResponse.GetResponseStream())
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            
+                            string errorText = await reader.ReadToEndAsync();
+                            var json = JToken.Parse(errorText);
+                            var errorMessage = (string)json["payload"]["message"]; 
+                             callback?.Invoke(json.ToString());
+                             return;
+                        }
+                    }
+                    GD.PrintErr("Erreur de requête HTTP: ", e.Message);
+                    callback?.Invoke(null);
+                }
+            }
+        }
+    }
